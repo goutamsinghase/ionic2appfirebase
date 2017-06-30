e4bad06609268afddf658cd996d69fc9f0933194
Goutam Singha <goutam.singha.cse@gmail.com>
2017-06-30 09:18:48 +0530

    email regex fixed

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -3,7 +3,7 @@ import { FormControl } from '@angular/forms';
 export class EmailValidator {
 
   static isValid(control: FormControl){
-    const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(control.value);
+    const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(control.value);
 
     if (re){ return null; }
     return { "invalidEmail": true };
